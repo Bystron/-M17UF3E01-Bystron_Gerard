c4ab939a493a3c314b31b6d9efe2381a0ad2614b
Gerard Bystron <gerard.bystroni.7e3@itb.cat>
2021-02-03 18:30:16 +0100

    Entrega

---
+++ M17UF3E01-Bystron_Gerard/Assets/activator.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activator : MonoBehaviour
+{
+    SpriteRenderer sr;
+    public KeyCode key;
+    bool active = false;
+    GameObject note;
+    Color old;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        old = sr.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(key))
+        {
+            StartCoroutine(Pressed());
+        }
+
+        if (Input.GetKeyDown(key) && active)
+        {
+            Destroy(note);
+            AddScore();
+            active = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        active = true;
+
+        if (collision.gameObject.tag == "Note")
+            note = collision.gameObject;
+            //Destroy(collision.gameObject);
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        active = false;
+    }
+
+    IEnumerator Pressed()
+    {
+        Color old = sr.color;
+        sr.color = new Color(0, 0, 0);
+        yield return new WaitForSeconds(0.05f);
+        sr.color = old;
+    }
+
+    void AddScore()
+    {
+        PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score") + 100);
+    }
+}
---
+++ M17UF3E01-Bystron_Gerard/Assets/note.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class note : MonoBehaviour
+{
+    Rigidbody2D rgdbd;
+    public float speed;
+
+    private void Awake()
+    {
+        rgdbd = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgdbd.velocity = new Vector2(0, -speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
